fec42f22c27430a0b23485756db54637a42f7c3c
Alexey Orlov <orlov@proitr.ru>
2022-05-19 20:51:30 +0300

    add auth

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/joho/godotenv"
 	"github.com/lelikptz/gitlab-webhook-notifier/internal/operation/webhook"
@@ -20,7 +21,7 @@ func init() {
 func main() {
 	log.Println("Start web server")
 
-	http.HandleFunc("/webhook", setupWebhook())
+	http.HandleFunc("/webhook", withAuth(setupWebhook()))
 	http.HandleFunc("/", response.NotFoundErrorResponse)
 
 	err := http.ListenAndServe(":8080", nil)
@@ -35,4 +36,15 @@ func setupWebhook() func(writer http.ResponseWriter, request *http.Request) {
 		*webhook.NewNotifier(),
 		*webhook.NewRequestParser(),
 	).Handle
+}
+
+func withAuth(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("X-Gitlab-Token")
+		if token == os.Getenv("GITLAB_TOKEN") {
+			next.ServeHTTP(w, r)
+			return
+		}
+		response.ErrorResponse("Unauthorized", http.StatusUnauthorized, w, r)
+	})
 }
